1573e20189dcc558efabe967431c7aec3c83a86f
Aditya Purandare <aditya.p1993@hotmail.com>
2015-11-22 03:15:12 -0600

    Create order ready functionally

--- createorder.php
+++ createorder.php
@@ -77,23 +77,29 @@ if(isset($_POST['search_query']) && !empty($_POST['search_query']))
 $itemlist = $array['items'];
 for ($i=0; $i < sizeof($array['items']) ; ++$i) {
 	//echo $itemlist[$i]['itemId'];
+	$itemname=$itemlist[$i]['name'];
+	$itemid=$itemlist[$i]['itemId'];
+	$itemimage=$itemlist[$i]['thumbnailImage'];
+	$itemprice = $itemlist[$i]['salePrice'];
+	$category=$itemlist[$i]['categoryPath'];
 
+//orderno, productcode, name, price, qunatity 
 	echo '<div class="product-information">
-								<h2>'.$itemlist[$i]['name'].'</h2>
-								<p>Item ID: '.$itemlist[$i]['itemId'].'</p>
-								<img src="'.$itemlist[$i]['thumbnailImage'].'" alt="" />
+								<h2>'.$itemname.'</h2>
+								<p>Item ID: '.$itemid.'</p>
+								<img src="'.$itemimage.'" alt="" />
 								<span>
-									<span>US $'.$itemlist[$i]['salePrice'].'</span>
+									<span>US $'.$itemprice.'</span>
 									<label>Quantity:</label>
-									<input type="text" value="1" />
+									<input type="text" value="1"  readonly="readonly"/>
 									<button type="button" class="btn btn-fefault cart">
-										<a href="additem.php"
+										<a href="additem.php?pid='.$itemid.'&name='.$itemname.'&price='.$itemprice.'&quantity=1" >
 										<i class="fa fa-shopping-cart"></i>
 										Select Item</a>
 									</button>
 								</span>
 								<p><b>Availability:</b> In Stock</p>
-								<p><b>Category:</b>'.$itemlist[$i]['categoryPath'].'</p>
+								<p><b>Category:</b>'.$category.'</p>
 							</div>';
 }
 ?>
@@ -118,80 +124,5 @@ for ($i=0; $i < sizeof($array['items']) ; ++$i) {
 		</div>
 	</section>
 
-	<footer id="footer"><!--Footer-->
-
-		<div class="footer-widget">
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-2">
-						<div class="single-widget">
-							<h2>Service</h2>
-							<ul class="nav nav-pills nav-stacked">
-								<li><a href="#">Online Help</a></li>
-								<li><a href="#">Contact Us</a></li>
-								<li><a href="#">Order Status</a></li>
-								<li><a href="#">Change Location</a></li>
-								<li><a href="#">FAQ’s</a></li>
-							</ul>
-						</div>
-					</div>
-					<div class="col-sm-2">
-						<div class="single-widget">
-							<h2>Policies</h2>
-							<ul class="nav nav-pills nav-stacked">
-								<li><a href="#">Terms of Use</a></li>
-								<li><a href="#">Privecy Policy</a></li>
-								<li><a href="#">Refund Policy</a></li>
-								<li><a href="#">Billing System</a></li>
-								<li><a href="#">Ticket System</a></li>
-							</ul>
-						</div>
-					</div>
-					<div class="col-sm-2">
-						<div class="single-widget">
-							<h2>About Shopper</h2>
-							<ul class="nav nav-pills nav-stacked">
-								<li><a href="#">Company Information</a></li>
-								<li><a href="#">Careers</a></li>
-								<li><a href="#">Store Location</a></li>
-								<li><a href="#">Affillate Program</a></li>
-								<li><a href="#">Copyright</a></li>
-							</ul>
-						</div>
-					</div>
-					<div class="col-sm-3 col-sm-offset-1">
-						<div class="single-widget">
-							<h2>About Shopper</h2>
-							<form action="#" class="searchform">
-								<input type="text" placeholder="Your email address" />
-								<button type="submit" class="btn btn-default"><i class="fa fa-arrow-circle-o-right"></i></button>
-								<p>Get the most recent updates from <br />our site and be updated your self...</p>
-							</form>
-						</div>
-					</div>
-
-				</div>
-			</div>
-		</div>
-
-		<div class="footer-bottom">
-			<div class="container">
-				<div class="row">
-					<p class="pull-left">Copyright © 2013 E-SHOPPER Inc. All rights reserved.</p>
-					<p class="pull-right">Designed by <span><a target="_blank" href="http://www.themeum.com">Themeum</a></span></p>
-				</div>
-			</div>
-		</div>
-
-	</footer><!--/Footer-->
-
-
-
-    <script src="js/jquery.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery.scrollUp.min.js"></script>
-	<script src="js/price-range.js"></script>
-    <script src="js/jquery.prettyPhoto.js"></script>
-    <script src="js/main.js"></script>
-</body>
+	</body>
 </html>
--- footer.php
+++ footer.php
@@ -1,10 +1,75 @@
-<div id="social">
-	<a href="http://www.facebook.com/renaissancepirates" class="anc"><img src="img/fb.png" alt="Eclectika Facebook Icon" /></a>
-	<a href="http://www.eclectika.org/index1.htm" class="anc"><img src="img/ec.png" alt="Eclectika Web Icon" /></a>
-	<a href="http://www.twitter.com/eclectika_nitrr" class="anc"><img src="img/twitter.png" alt="Eclectika Twitter Icon" /></a>
-	<p style="margin-bottom: -20px" class="bold">
-		&copy; 2014 Created by Aditya Purandare, Krishna Srivastava and Mansi Jain.
-	</p>
-</div>
-<?php include_once("analyticstracking.php"); ?>
-<img src="img/footer.png" alt="" width="100%" height="150px"/>
+<footer id="footer"><!--Footer-->
+
+		<div class="footer-widget">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>Service</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Online Help</a></li>
+								<li><a href="#">Contact Us</a></li>
+								<li><a href="#">Order Status</a></li>
+								<li><a href="#">Change Location</a></li>
+								<li><a href="#">FAQ’s</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>Policies</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Terms of Use</a></li>
+								<li><a href="#">Privecy Policy</a></li>
+								<li><a href="#">Refund Policy</a></li>
+								<li><a href="#">Billing System</a></li>
+								<li><a href="#">Ticket System</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>About Shopper</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Company Information</a></li>
+								<li><a href="#">Careers</a></li>
+								<li><a href="#">Store Location</a></li>
+								<li><a href="#">Affillate Program</a></li>
+								<li><a href="#">Copyright</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-3 col-sm-offset-1">
+						<div class="single-widget">
+							<h2>About Shopper</h2>
+							<form action="#" class="searchform">
+								<input type="text" placeholder="Your email address" />
+								<button type="submit" class="btn btn-default"><i class="fa fa-arrow-circle-o-right"></i></button>
+								<p>Get the most recent updates from <br />our site and be updated your self...</p>
+							</form>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+		<div class="footer-bottom">
+			<div class="container">
+				<div class="row">
+					<p class="pull-left">Copyright © 2013 E-SHOPPER Inc. All rights reserved.</p>
+					<p class="pull-right">Designed by <span><a target="_blank" href="http://www.themeum.com">Themeum</a></span></p>
+				</div>
+			</div>
+		</div>
+
+	</footer><!--/Footer-->
+
+
+
+    <script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.scrollUp.min.js"></script>
+	<script src="js/price-range.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/main.js"></script>
