ddaba10f0fbe1625b412999ca0309e362259cd6a
Aditya Purandare <aditya.p1993@hotmail.com>
2015-11-22 04:11:34 -0600

    Adding item

---
+++ additem.php
@@ -0,0 +1,38 @@
+<?php
+ob_start();
+require('connect.php');
+
+if($_GET)
+{
+	$pcode=$_GET['pid'];
+	$name=$_GET['name'];
+	$price=$_GET['price'];
+	$qty=$_GET['quantity']);
+	$creatorid=$_SESSION['userid'];
+	
+	$query="SELECT orderNumber FROM Orders WHERE status='cart'";
+	$res=mysqli_query($con,$query);
+	if(mysqli_num_rows($res)>0)
+	{
+		$row=mysqli_fetch_array($res);
+		$ordernum=$row['orderNumber'];
+	}
+	
+	if(isset($pid)&&isset($name)&&isset($price)&&isset($qty))
+	{
+		if($ordernum)
+		{
+			$q="INSERT INTO (productCode, orderNumber, quantityOrdered, order_creator_id, priceEach, productName) VALUES ('$pcode', '$ordernum','$qty','$creatorid', '$price', '$name')";
+			}
+			else
+			{
+		$q="INSERT INTO (productCode, orderNumber, quantityOrdered, order_creator_id, priceEach, productName) VALUES ('$pcode','','$qty','$creatorid', '$price', '$name')";
+		}
+		echo $q;
+		//mysqli_query($con, $q);
+		echo "Item added successfully";
+		header('location:createorder.php');
+	}
+}
+
+?>
