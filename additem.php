1e463d3ac3789fdb9ce5324cd6cdd8d10d99984b
Aditya Purandare <aditya.p1993@hotmail.com>
2015-11-22 09:51:57 -0600

    Login panel changed #32

--- additem.php
+++ additem.php
@@ -11,27 +11,30 @@ if($_GET)
 	$qty=mysqli_real_escape_string($con,$_GET['quantity']);
 	$creatorid=$_SESSION['userid'];
 	
-	$query="SELECT orderNumber FROM Orders WHERE status='cart'";
-	echo $query;die();
+	$query="SELECT orderNumber FROM Orders WHERE status='cart' AND order_creator_id='$creatorid'";
+	//echo $query;die();
 	$res=mysqli_query($con,$query);
 	if(mysqli_num_rows($res)>0)
 	{
 		$row=mysqli_fetch_array($res);
 		$ordernum=$row['orderNumber'];
 	}
+	else
+	{
+		$query1="SELECT max(orderNumber) FROM Orders";
+		$res1=mysqli_query($con,$query);
+		$row=mysqli_fetch_array($res1)
+		$ordernum=$row[0];
+	}
 	echo $pcode.'<br/>'.$name.'<br/>'.$price.'<br/>'.$qty;
 	
 	if(isset($pcode)&&isset($name)&&isset($price)&&isset($qty))
 	{
-		if($ordernum)
-		{
+
 			$q="INSERT INTO orderdetails(productCode, orderNumber, quantityOrdered, order_creator_id, priceEach, productName) VALUES ('$pcode', '$ordernum','$qty','$creatorid', '$price', '$name')";
-			}
-			else
-			{
-		$q="INSERT INTO orderdetails(productCode, orderNumber, quantityOrdered, order_creator_id, priceEach, productName) VALUES ('$pcode','','$qty','$creatorid', '$price', '$name')";
+			
 		}
-		//echo $q;die();
+		echo $q;die();
 		mysqli_query($con, $q);
 		echo "Item added successfully";
 		header('location:createorder.php');
