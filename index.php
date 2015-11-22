aeb7db994e23923b723d11cdb5bc79000760c4e4
Aditya Purandare <aditya.p1993@hotmail.com>
2015-11-22 12:02:37 -0600

    Index modification #2

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Home | E-Shopper</title>
+    <title>Home | BuyMyGroc</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
     <link href="css/prettyPhoto.css" rel="stylesheet">
