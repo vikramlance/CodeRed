3d69aef128e0dbc3cfd47d846b12fe8da618a0ba
Aditya Purandare <aditya.p1993@hotmail.com>
2015-11-22 01:04:53 -0600

    PhP Pages update #1

---
+++ createorder.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+	<?php require('header.php'); ?>
+		
+		<div class="header-middle"><!--header-middle-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="logo pull-left">
+							<a href="index.html"><img src="images/home/logo.png" alt="" /></a>
+						</div>
+
+					</div>
+					<div class="col-sm-8">
+						<div class="shop-menu pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href="#"><i class="fa fa-user"></i> Create</a></li>
+								<li><a href="#"><i class="fa fa-star"></i> Discover</a></li>
+								<li><a href="checkout.html"><i class="fa fa-crosshairs"></i> How it works</a></li>
+								
+								<li><a href="login.html"><i class="fa fa-lock"></i> Login</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header-middle-->
+	
+
+	
+	<section>
+		<div class="container">
+			<div class="row">
+
+							<div class="col-sm-9 padding-right">
+					<div class="product-details"><!--product-details-->
+						<div class="col-sm-5">
+							<div class="view-product">
+								<h3>ZOOM</h3>
+							</div>
+						</div>
+						<div class="col-sm-7">
+							<div class="product-information"><!--/product-information-->
+								<img src="images/product-details/new.jpg" class="newarrival" alt="" />
+								<h2>Anne Klein Sleeveless Colorblock Scuba</h2>
+								<p>Web ID: 1089772</p>
+								<img src="images/product-details/rating.png" alt="" />
+								<span>
+									<span>US $59</span>
+									<label>Quantity:</label>
+									<input type="text" value="3" />
+									<button type="button" class="btn btn-fefault cart">
+										<i class="fa fa-shopping-cart"></i>
+										Add to cart
+									</button>
+								</span>
+								<p><b>Availability:</b> In Stock</p>
+								<p><b>Condition:</b> New</p>
+								<p><b>Brand:</b> E-SHOPPER</p>
+								<a href=""><img src="images/product-details/share.png" class="share img-responsive"  alt="" /></a>
+							</div><!--/product-information-->
+						</div>
+					</div><!--/product-details-->
+
+			
+			</div>
+		</div>
+	</section>
+	
+
+		<div class="container">
+			<div class="row">
+				
+				<div class="col-sm-9 padding-right">
+					
+					
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<footer id="footer"><!--Footer-->
+
+		<div class="footer-widget">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>Service</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Online Help</a></li>
+								<li><a href="#">Contact Us</a></li>
+								<li><a href="#">Order Status</a></li>
+								<li><a href="#">Change Location</a></li>
+								<li><a href="#">FAQ’s</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>Policies</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Terms of Use</a></li>
+								<li><a href="#">Privecy Policy</a></li>
+								<li><a href="#">Refund Policy</a></li>
+								<li><a href="#">Billing System</a></li>
+								<li><a href="#">Ticket System</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>About Shopper</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Company Information</a></li>
+								<li><a href="#">Careers</a></li>
+								<li><a href="#">Store Location</a></li>
+								<li><a href="#">Affillate Program</a></li>
+								<li><a href="#">Copyright</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-3 col-sm-offset-1">
+						<div class="single-widget">
+							<h2>About Shopper</h2>
+							<form action="#" class="searchform">
+								<input type="text" placeholder="Your email address" />
+								<button type="submit" class="btn btn-default"><i class="fa fa-arrow-circle-o-right"></i></button>
+								<p>Get the most recent updates from <br />our site and be updated your self...</p>
+							</form>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+		
+		<div class="footer-bottom">
+			<div class="container">
+				<div class="row">
+					<p class="pull-left">Copyright © 2013 E-SHOPPER Inc. All rights reserved.</p>
+					<p class="pull-right">Designed by <span><a target="_blank" href="http://www.themeum.com">Themeum</a></span></p>
+				</div>
+			</div>
+		</div>
+		
+	</footer><!--/Footer-->
+	
+
+  
+    <script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.scrollUp.min.js"></script>
+	<script src="js/price-range.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,72 +1,56 @@
 <?php
-	ob_start();
-	require("connect.php");
-	if(!isset($_SESSION['username']))
-    {
-        header('location:login.php');
-    }
-    else
-    {
-        //Proceed with Game Home
-    }
 
-	$username=$_SESSION['username'];
-	$q8="SELECT name, score, pic FROM stuinfo, quizuser WHERE stuinfo.username=quizuser.username AND quizuser.username='$username'";
-	$r8=mysqli_query($con, $q8);
-	if(mysqli_num_rows($r8)==1)
-	{
-		$row=mysqli_fetch_array($r8);
-		$name=$row['name'];
-		$score=$row['score'];
-		$q9="SELECT count(id) as rank FROM quizuser WHERE score>$score ORDER BY time ASC";
-		$r9=mysqli_query($con,$q9);
-		$q91="SELECT count(id)as total FROM quizuser";
-		$r91=mysqli_query($con, $q91);
-		$row1=mysqli_fetch_array($r9);
-		$row2=mysqli_fetch_array($r91);
-		$total=$row2['total'];
-		$rank=$row1['rank']+1;
-		$ahead=$rank-1;
-		$behind=$total-$rank;
-		$ahead=($ahead/$total)*100;
-		$behind=($behind/$total)*100;
-		$ahead=round( $ahead, 2, PHP_ROUND_HALF_UP);
-		$behind=round( $behind, 2, PHP_ROUND_HALF_UP);
-	}
+echo '<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Home | E-Shopper</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/price-range.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
 
+<body>
+	<header id="header"><!--header-->
+		<div class="header_top"><!--header_top-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6">
+						<div class="contactinfo">
+							<ul class="nav nav-pills">
+								<li><a href="#"><i class="fa fa-phone"></i> +2 95 01 88 821</a></li>
+								<li><a href="#"><i class="fa fa-envelope"></i> info@buymegroc.com</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="social-icons pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+								<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+								<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--/header_top-->
+		';
 ?>
-<ul id="navbar">
-	<li id="home"><a href="home.php">Home</a></li>
-	<li id="profile"><a href="profile.php">My Profile</a></li>
-	<li id="play"><a href="home.php">Renaissance Pirates</a></li>
-	<li id="rules"><a href="rules.php">Rules</a></li>
-	<li id="contacts"><a href="contact.php">Contacts</a></li>
-</ul>
-<div id="userdetails">
-<table width="80%" >
-	<tr>
-		<td width="27%" align="right"> Name - <span class="bold"><?php echo $name; ?></span></td>
-		<td width="6%">Rank - <span class="bold"><?php echo $rank; ?></span></td>
-		<td width="6%">Score - <span class="bold"><?php echo $score; ?></span></td>
-		<td width="12%" align="center"><span class="bold "><a href="logout.php" class="logout">LOGOUT</a></span></td>
-	</tr>
-	<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-	<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
-	<tr>
-		<td>
-		<?php
-					if($row['pic']!=null)
-						echo '<img src="'.$row['pic'].'" alt="Profile Picture" height="80" width="80"/>';
-					else
-						echo '<img src="img/default.gif" alt="Profile Picture" height="80" width="80"/>';
-		?>
-		</td>
-		<td width="22%">
-				<?php echo '<em>Users Ahead of You :</em><strong> '.$ahead.'%</strong>'; ?>
-		</td>
-		<td width="20%">
-				<?php echo '<em>Users Behind You :</em><strong> '.$behind.'%</strong>'; ?>
-		</td>
-	</tr>
-</table>
-</div>
