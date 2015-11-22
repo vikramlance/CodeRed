693c6d04167581b46ba5ec10c077f9bdcc3b5627
Aditya Purandare <aditya.p1993@hotmail.com>
2015-11-22 08:35:47 -0600

    Login panel changed #8

--- createorder.php
+++ createorder.php
@@ -7,18 +7,18 @@
 				<div class="row">
 					<div class="col-sm-4">
 						<div class="logo pull-left">
-							<a href="index.html"><img src="images/home/logo.png" alt="" /></a>
+							<a href="index.php"><img src="images/home/logo.png" alt="" /></a>
 						</div>
 
 					</div>
 					<div class="col-sm-8">
 						<div class="shop-menu pull-right">
 							<ul class="nav navbar-nav">
-								<li><a href="#"><i class="fa fa-user"></i> Create</a></li>
-								<li><a href="#"><i class="fa fa-star"></i> Discover</a></li>
-								<li><a href="checkout.html"><i class="fa fa-crosshairs"></i> How it works</a></li>
+								<li><a href="createorder.php"><i class="fa fa-user"></i> Create</a></li>
+								<li><a href="discover.php"><i class="fa fa-star"></i> Discover</a></li>
+								<li><a href="checkout.php"><i class="fa fa-crosshairs"></i> How it works</a></li>
 
-								<li><a href="login.html"><i class="fa fa-lock"></i> Login</a></li>
+								<li><a href="login.php"><i class="fa fa-lock"></i> Login</a></li>
 							</ul>
 						</div>
 					</div>
