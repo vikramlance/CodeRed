d77fbc5f27914719150056f8b6e8689755aabe98
Aditya Purandare <aditya.p1993@hotmail.com>
2015-11-22 09:05:36 -0600

    Login panel changed #20

--- createorder.php
+++ createorder.php
@@ -109,7 +109,7 @@ for ($i=0; $i < sizeof($array['items']) ; ++$i) {
 ?>
 <?php
 	$userid=$_SESSION['userid'];
-	$squery="SELECT * FROM orderdetails WHERE order_creator_id='$userid'";
+	$squery="SELECT * FROM orderdetails, orders WHERE orderdetails.order_creator_id='$userid' AND orders.status <> 'completed'";
 	echo $squery;
 	$sresult=mysqli_query($con, $squery);
 	
