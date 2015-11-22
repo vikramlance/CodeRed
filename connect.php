8e389d3ee0765238c382100100d1c8e544daf25e
Aditya Purandare <aditya.p1993@hotmail.com>
2015-11-22 04:12:45 -0600

    Connection established

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 
-	$host="localhost";
-	$user="root";
-	$pass="";
+	$host="us-cdbr-azure-west-c.cloudapp.net";
+	$user="bd0e3aeafe641e";
+	$pass="1e8243d1";
 	$dbname="codereddb";
 	
 	$con=mysqli_connect($host, $user, $pass, $dbname);
